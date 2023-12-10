f20c75fb822bf8bf9ae1bd41bcf1d94235c46be7
Carl Storms <37883693+TheBIMsider@users.noreply.github.com>
2023-12-10 13:31:02 -0700

    Delete Day10 directory

--- Day10/index.js
+++
@@ -1,8 +0,0 @@
-// JavaScriptmas Day 10
-  // Challenge: Add code here to make the youtube player play the new YouTube song
-
-const player = document.getElementById("player")
-
-function playSong(id) {
-  player.src = `https://www.youtube.com/embed/${id}?autoplay=1`
-}
